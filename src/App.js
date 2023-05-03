3851d73f3dd6e420d492166716c70c55618d9bb3
its-vikash <dev.vikashpatel@gmail.com>
2023-05-03 15:15:19 +0530

    Updated Restro4Foodies App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Restaurant from './components/basics/restaurant';
+
+const App = () => {
+  return (
+    <>
+    <Restaurant/>
+    <div style={{color:'green', textAlign:"center", fontSize:'20px', marginTop:'50px', backgroundColor:'lightyellow'}}>Copyright © 2023 Restaurant Menu ~ Thanks @Thapa Technical</div>
+    <div style={{color:'green', textAlign:"center", fontSize:'20px', marginTop:'20px', backgroundColor:'lightyellow'}}>Terms & Conditions |
+Privacy Policy |
+All Rights Reserved |
+Cookies Policy</div>
+    <div style={{marginTop:'50px'}}></div>
+    </>
+  )
+}
+
+export default App
